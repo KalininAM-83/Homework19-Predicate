d9fba7cfed8c1f92cff155f0bb1f03835141f729
KalininAM-83 <alexch05@mail.ru>
2025-03-14 19:40:41 +0300

    Homework19 - Predicate

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,24 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(-5, 6, 9, 0, -10, 15, 43, 86, -20, 11, 3, -1);
 
-        System.out.println("\nПроверка с помощью предиката через отдельных класс");
+        System.out.println("\nПроверим есть ли в списке отрицательные числа с помощью предиката через отдельный класс");
         NumberPredicate predicate = new NumberPredicate();
         for (Integer number : numbers) {
             if (predicate.test(number)) {
                 System.out.println(number);
             }
         }
-
-        System.out.println("\nПроверка с помощью предиката через анонимную функцию");
+//      тоже самое через stream
+        System.out.println("\nПроверим есть ли в списке отрицательные числа с помощью Stream + Predicate");
         numbers
                 .stream()
                 .filter(predicate)
+                .forEach(System.out::println);
+
+        System.out.println("\nПроверим есть ли в списке числа в диапазоне от - до через анонимную функцию");
+        numbers
+                .stream()
+                .filter(number -> number >= -5 && number <= 15)
                 .forEach(System.out::println);
     }
 }
--- src/NumberPredicate.java
+++ src/NumberPredicate.java
@@ -3,12 +3,7 @@ import java.util.function.Predicate;
 public class NumberPredicate implements Predicate<Integer> {
     @Override
     public boolean test(Integer numbers) {
-        //проверка числа на отрицательное значение
-        if (numbers < 0) {
-            return true;
-        }
-        //проверка числа, находится ли оно в диапазоне от - до
-        return numbers >= -5 && numbers <= 15;
+        return numbers < 0;
 
     }
 }
